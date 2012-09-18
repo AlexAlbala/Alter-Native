6c421ef611541cd4651a7eab186dfa6fc924630c
AlexAlbala <alexalbala88@gmail.com>
2012-09-18 11:48:37 +0200

    ---Removed unused code +++Added external libraries source code (compressed) +++Added boost library includes and libs

--- Lib/System/Text/UTF8Encoding.cpp
+++ Lib/System/Text/UTF8Encoding.cpp
@@ -1,22 +1,42 @@
 #include "UTF8Encoding.h"
+#include <boost/locale.hpp>
 
+namespace loc = boost::locale;
 namespace System{
 	namespace Text{
 
 		Array<char>* UTF8Encoding::GetBytes(String* value)
 		{
-			Array<char>* v = new Array<char>(value->Data,value->Length);
+			loc::generator* gen = new loc::generator();
+			std::locale blah = gen->generate("es_ES.utf-8");
+			std::string* UTF8String = new string(value->Data);
+
+			std::string converted = loc::conv::from_utf(*UTF8String, blah);
+
+			delete gen;
+			delete UTF8String;
+
+			Array<char>* v = new Array<char>((char*)converted.data(),converted.size());
 			return v;
 		}
 
 		String* UTF8Encoding::GetString(char* bytes)
 		{
-			return new String(bytes);
+			return GetString(new Array<char>(bytes, strlen(bytes)));
 		}
 
 		String* UTF8Encoding::GetString(Array<char>* bytes)
-		{
-			return new String(bytes->GetData());
+		{	
+			loc::generator* gen = new loc::generator();
+			std::locale blah = gen->generate("es_ES.utf-8");
+			std::string* UTF8String = new string(bytes->GetData());
+
+			std::string converted = loc::conv::from_utf(*UTF8String, blah);
+
+			delete gen;
+			delete UTF8String;
+
+			return new String(converted.data());
 		}
 	}
 }
