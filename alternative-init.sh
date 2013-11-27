9887cbba35202b53eb5fa190b92f081e29b99395
Alex Albalá Díaz <alejandro.albala@upc.edu>
2013-11-27 15:33:05 +0100

    Update alternative-init.sh
    fixed wrong path

--- alternative-init.sh
+++ alternative-init.sh
@@ -7,7 +7,7 @@ declare -x ALTERNATIVE_HOME=`pwd`
 declare -x ALTERNATIVE_BIN_PATH=$ALTERNATIVE_HOME/AlterNative.Core/AlterNative.Core/bin/Debug
 
 # CPP_LIB_PATH
-declare -x ALTERNATIVE_CPP_LIB_PATH=$ALTERNATIVE_HOME/AlterNative.Core/Lib
+declare -x ALTERNATIVE_CPP_LIB_PATH=$ALTERNATIVE_HOME/Lib/src
 
 # Shell Scripts
 PATH=$PATH:$ALTERNATIVE_HOME/Tools/ShellScripts
