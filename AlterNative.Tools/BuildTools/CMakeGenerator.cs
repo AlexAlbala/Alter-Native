fd908eea285aadd4fe63f397ee98491eaa398653
AlexAlbala <alexalbala88@gmail.com>
2014-02-06 17:36:38 +0100

    fixed path bug

--- AlterNative.Tools/BuildTools/CMakeGenerator.cs
+++ AlterNative.Tools/BuildTools/CMakeGenerator.cs
@@ -55,13 +55,10 @@ namespace AlterNative.BuildTools
             sb.AppendLine("ENDIF(UNIX)");
 
             if (release)
-                sb.AppendLine("SET(CMAKE_BUILD_TYPE Release)");
+                sb.AppendLine("SET(CMAKE_BUILD_TYPE Release)");            
 
-#if CORE
-            FileInfo boostCmake = new FileInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"/../../../../Tools/Code/CMAKE-BOOST");
-#else
             FileInfo boostCmake = new FileInfo(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"/../../../Tools/Code/CMAKE-BOOST");
-#endif
+
             StreamReader sr = new StreamReader(boostCmake.FullName);
             sb.AppendLine("SET(PROJ_NAME " + execName + ")");
             sb.Append(sr.ReadToEnd());
