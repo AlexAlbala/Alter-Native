3eee9ff9e30debdb4341d31b4785df6c5b010295
AlexAlbala <alexalbala88@gmail.com>
2014-05-16 10:01:57 +0200

    +++ fixed install.bat for windows environments

--- install.bat
+++ install.bat
@@ -1,9 +1,13 @@
-alternative-init.bat
+@ECHO off
 
-cd %ALTERNATIVE_LIB_CPP_PATH%
-alternative-lib-compile.bat
+call alternative-init.bat
+
+cd %ALTERNATIVE_CPP_LIB_PATH%
+call alternative-lib-compile.bat
 
 ::The script alternative-lib-compile.bat sets a variable msbuildPath useful for this script
 
 cd %msbuildPath%
 msbuild.exe %ALTERNATIVE_HOME%/Alternative.sln /p:Configuration=net_4_0_Debug
+
+cd %ALTERNATIVE_HOME%
