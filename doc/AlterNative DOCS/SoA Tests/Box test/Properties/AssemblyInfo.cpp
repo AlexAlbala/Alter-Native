ebfc38c8ea844f3372ee21cc0ed559428facb576
AlexAlbala <alexalbala88@gmail.com>
2014-05-20 09:13:18 +0200

    +++ moved documentation

--- doc/AlterNative DOCS/SoA Tests/Box test/Properties/AssemblyInfo.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "AssemblyInfo.h"
-
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-//using namespace System::Reflection;
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-//using namespace System::Runtime::CompilerServices;
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-//using namespace System::Runtime::InteropServices;
