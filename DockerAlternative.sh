f754ad17d051392a9f3b2eba44c944376feb1b0c
Gerard <gerard.sole.castellvi@gmail.com>
2014-06-29 18:57:02 +0200

    Create DockerAlternative.sh

---
+++ DockerAlternative.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# ALTERNATIVE_PATH
+export ALTERNATIVE_HOME=`pwd`
+
+# ALTERNATIVE_BIN_PATH
+export ALTERNATIVE_BIN_PATH=$ALTERNATIVE_HOME/AlterNative.Core.bin/bin/Debug
+
+# ALTERNATIVE_BIN_PATH
+export ALTERNATIVE_BIN=$ALTERNATIVE_HOME/AlterNative.Core.bin/bin/Debug/AlterNative.Core.exe
+
+# CPP_LIB_PATH
+export ALTERNATIVE_CPP_LIB_PATH=$ALTERNATIVE_HOME/Lib
+
+# ALTERNATIVE_TOOLS
+export ALTERNATIVE_TOOLS_PATH=$ALTERNATIVE_HOME/Tools
+
+# Shell Scripts
+export PATH=$PATH:$ALTERNATIVE_HOME/Tools/ShellScripts
+
+chmod +x $ALTERNATIVE_HOME/Tools/ShellScripts/alternative
