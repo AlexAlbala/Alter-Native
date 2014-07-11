c9bba8ddafd22b679eb131dc81eaa8508efd7248
Gerard <gerard.sole.castellvi@gmail.com>
2014-07-11 14:34:55 +0200

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-# ALTERNATIVE_PATH
-declare -x ALTERNATIVE_HOME=`pwd`
-
-# ALTERNATIVE_BIN_PATH
-declare -x ALTERNATIVE_BIN_PATH=$ALTERNATIVE_HOME/AlterNative.Core.bin/bin/Debug
-
-# ALTERNATIVE_BIN_PATH
-declare -x ALTERNATIVE_BIN=$ALTERNATIVE_HOME/AlterNative.Core.bin/bin/Debug/AlterNative.Core.exe
-
-# CPP_LIB_PATH
-declare -x ALTERNATIVE_CPP_LIB_PATH=$ALTERNATIVE_HOME/Lib
-
-# ALTERNATIVE_TOOLS
-declare -x ALTERNATIVE_TOOLS_PATH=$ALTERNATIVE_HOME/Tools
-
-# Shell Scripts
-declare -x PATH=$PATH:$ALTERNATIVE_HOME/Tools/ShellScripts
-
 chmod +x $ALTERNATIVE_HOME/Tools/ShellScripts/alternative
 
 xbuild AlterNative.Core.sln /t:Clean
